1b01de9624d84206594c6f7c8e9dc206416e4518
MichaelCook <michael@evoluted.net>
2017-08-15 01:22:04 +0100

    FINALLY GOT SOMEWHERE! fullscreen and orientation lock

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 let http = require('http'),
   fs = require('fs'),
   port = 80;
@@ -5,29 +6,46 @@ let http = require('http'),
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
-fs.readFile('./public/client.html', function(err, html) {
-  if (err) {
-    throw err;
-  }
+let readFilePromise = filename => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (error, data) => {
+      if (error) {
+        console.log('Error: ' + error);
+        reject(error);
+      }
+      resolve(data);
+    });
+  });
+};
 
+Promise.all([
+  readFilePromise('./public/client.html'),
+  readFilePromise('./public/manifest.json')
+]).then(resolutions => {
+  let client = resolutions[0],
+    manifest = resolutions[1];
   // Net server (or http would be fine) to serve the page
   http
     .createServer(function(request, response) {
       console.log('User connected');
 
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(html);
+      if (request.url == '/manifest.json') {
+        console.log(manifest);
+        response.write(manifest.toString());
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(client.toString());
+      }
       response.end();
     })
     .listen(port);
   console.log('Listening on http://192.168.3.1:' + port);
-
-  // WebSocket server for sockets
-  wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-      console.log(message);
-    });
-
-    ws.send('Hello from server!');
+});
+// WebSocket server for sockets
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log(message);
   });
+
+  ws.send('Hello from server!');
 });
